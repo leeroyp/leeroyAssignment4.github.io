4894ac77b19660eedf7a7ee2db418b5267525251
Leeroy Phili <leeroyphili@gmail.com>
2020-02-16 15:10:26 -0500

    first commit

---
+++ questions.js
@@ -0,0 +1,103 @@
+const startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startGame)
+
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.choise = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+
+function populate() {
+    if(quiz.isEnded()) {
+        alert("game over");
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            choise("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function choise(id, choise) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.choise(choise);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+function startGame() {
+	startButton.classList.add("hide");
+	
+}
+// create questions here
+var questions = [
+    new Question("What languages hasn't been covered in the coding bootcamp?", ["JavaScript", "XHTML","CSS", "React"], "React"),
+    new Question("what is Leeroy's last name ?", ["Philips", "Leeroy","Phili", "Ghazi"], "Phili"),
+    new Question("which of the following is used to send content to github?", ["git pull", "git push","git clone", "git init"], "git push"),
+    new Question("Hyper Text Markup Language Stand For?", ["JavaScript", "XHTML","CSS", "HTML"], "HTML"),
+    new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
+    new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
+    new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
